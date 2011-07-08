2b344daabe8503546ad573b7b1ad9e3daf048ff5
Pedro Putz <pedroputz@gmail.com>
2011-07-08 16:30:26 -0300

    problem 4

---
+++ problem-0004.rb
@@ -0,0 +1,23 @@
+class Fixnum
+  def palindrome?
+    if self.to_s == self.to_s.reverse
+      return true
+    else
+      return false
+    end
+  end
+end
+
+value01, value02 = 999, 999
+numbers = []
+
+begin
+  if (value01 * value02).palindrome?
+    numbers << value01 * value02 if (value01 * value02).to_s.size() == 6
+  end
+
+  value02 -= 1 if value01 == 101
+  value01 = (value01 == 101) ? (999) : (value01 - 1)
+end while value02 > 100
+
+puts numbers.uniq.sort.last
