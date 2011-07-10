95bc59c699250297fa2d69d8707f7e1309a595c2
Pedro Putz <pedroputz@gmail.com>
2011-07-10 20:22:12 -0300

    problem 10 - adding start and end time

--- problem-0010.rb
+++ problem-0010.rb
@@ -2,6 +2,8 @@ value = 2000000
 limit = Math.sqrt(value).to_i
 primeNumbers, number, item, sum = [2], 3, 0, 0
 
+puts Time.utc(*Time.new.to_a)
+
 while number < value
   primeNumbers << number
   number +=2
@@ -15,4 +17,5 @@ end
 
 primeNumbers.each { |value| sum += value }
 
+puts Time.utc(*Time.new.to_a)
 puts sum
