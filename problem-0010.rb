a396bda0307eac2e528801c3d626912212434e80
Pedro Putz <pedroputz@gmail.com>
2011-07-11 23:11:54 -0300

    problem 10 - moving the script to mathfunctions

--- problem-0010.rb
+++ problem-0010.rb
@@ -1,21 +1,10 @@
-value = 2000000
-limit = Math.sqrt(value).to_i
-primeNumbers, number, item, sum = [2], 3, 0, 0
+require_relative 'support/mathfunctions'
 
 puts Time.utc(*Time.new.to_a)
 
-while number < value
-  primeNumbers << number
-  number +=2
-end
-
-while primeNumbers[item] <= limit
-  prime = primeNumbers[item]
-  primeNumbers.delete_if { |value| value > prime && value % prime == 0 }
-  item += 1
-end
-
-primeNumbers.each { |value| sum += value }
+sum = 0
+primes = 2000000.total_of_primes
+primes.each { |value| sum += value }
 
 puts Time.utc(*Time.new.to_a)
 puts sum
--- support/mathfunctions.rb
+++ support/mathfunctions.rb
@@ -39,4 +39,23 @@ class Fixnum
 
     return primes
   end
+
+  def total_of_primes
+    value = self
+    limit = Math.sqrt(value).to_i
+    primeNumbers, number, item, sum = [2], 3, 0, 0
+
+    while number < value
+      primeNumbers << number
+      number +=2
+    end
+
+    while primeNumbers[item] <= limit
+      prime = primeNumbers[item]
+      primeNumbers.delete_if { |value| value > prime && value % prime == 0 }
+      item += 1
+    end
+
+    return primeNumbers
+  end
 end
