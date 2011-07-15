9d9ae2027b836b733173dc4f407ea8b4072da8b4
Pedro Putz <pedroputz@gmail.com>
2011-07-14 23:35:54 -0300

    problem 1 - better def name

--- problem-0001.rb
+++ problem-0001.rb
@@ -1,13 +1,13 @@
 require_relative 'support/benchmark'
 
-def answer
+def sum_of_all_the_multiples_of_3_or_5_below (limit)
   sum = 0
 
-  3.upto(999) do |number|
+  3.upto(limit) do |number|
     sum += number if number % 3 == 0 || number % 5 == 0
   end
 
   return sum
 end
 
-benchmark { answer }
+benchmark { sum_of_all_the_multiples_of_3_or_5_below 999 }
